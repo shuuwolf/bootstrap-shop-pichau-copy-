903a460ef7cfaeb76453f47b2a5603e4237c815e
Rafael Kauch <64663811+shuuwolf@users.noreply.github.com>
2021-07-06 21:29:15 -0300

    iniciei o menu da lateral(nav_offcanvas)

--- js/function.js
+++ js/function.js
@@ -4,5 +4,18 @@ $(function(){
         $( "#boxes" ).remove();
         $( "#new_product" ).remove();
         
+        const xhr = new XMLHttpRequest();
+        const myCar = document.getElementById('myCar');
+
+        xhr.onload = function(){
+          if(this.status === 200){ //1xx: informational requests, 2xx: successuful requests, 3xx: redirects, 4xx: client errors, 5xx: server errors
+            myCar.innerHTML = xhr.responseText;//adiciona o request dentro da id myCar
+          }else{
+            console.warn('Did not receive 200 OK from server');
+          }
+        };
+
+        xhr.open('get', 'cart.html');//usa o metodo GET para pegar o cart.html
+        xhr.send();
       });
 })
